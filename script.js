4d4b4d5dfbc68ff4d44a4cccfc2c847679c327f1
Jonas Bøgh <jonasbogh@Jonass-MacBook-Pro-2.local>
2020-01-07 03:58:53 +0100

    javascript - burgermenu kode

--- script.js
+++ script.js
@@ -2,4 +2,18 @@ window.addEventListener("load", init);
 
 function init() {
     console.log("init");
+    document.querySelector(".icon").addEventListener("click", clickFunction);
 }
+
+
+function clickFunction() {
+    console.log("clickFunction");
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+//Hvis "myLinks" har display "block" så skal display stylen være none: ellers skal den være block.
